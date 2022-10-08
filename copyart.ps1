81f9237a2746df462f4d9a8a59ee8794ddca8f00
NallamuthuK91 <98209715+NallamuthuK91@users.noreply.github.com>
2022-10-08 22:18:36 +0530

    Add files via upload

---
+++ copyart.ps1
@@ -0,0 +1,7 @@
+﻿# Script to copy the *.war to Dest
+
+$releaseartifact = C:\Windows\System32\agent\_work\r1\a\_NallamuthuK91.DevopsBasics\ToPipeline\webapp\target\webapp.war
+
+$destartifact = C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps\
+
+Copy-Item $releaseartifact -Destination $destartifact
